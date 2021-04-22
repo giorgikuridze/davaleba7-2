c3eb9edd94e75a388798c8379076933988b235a3
giorgikuridze <giorgi.kuridze.1@btu.edu.ge>
2021-04-22 17:39:54 +0400

    sleep

--- hungry.py
+++ hungry.py
@@ -3,6 +3,10 @@ if hungry=="yes":
 	print("eat potato")
 	print("eat pizza")
 elif:
-	thirsty=input("are you thirsty")
+	thirsty=input("are you thirsty?")
 	if thirsty=="yes":
-		print("drink water");
+		print("drink water")
+else:
+	tired=input("are you tired?")
+	if tired=="yes":
+		print("go to sleep")
