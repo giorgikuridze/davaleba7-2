efa38abb017faa7aaf2b8a583976cae5a4954acb
giorgikuridze <giorgi.kuridze.1@btu.edu.ge>
2021-04-22 17:38:28 +0400

    water

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry=input("are you hungry?")
 if hungry=="yes":
 	print("eat potato")
 	print("eat pizza")
-else:
-	print("do your homework")
+elif:
+	thirsty=input("are you thirsty")
+	if thirsty=="yes":
+		print("drink water");
